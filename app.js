bf20d8268cdf71f9042522736f7fec6ac0db1141
Andriyansyah0003 <andriyansyah0003@gmail.com>
2020-12-23 13:09:51 +0700

    setdefault latitude dan longitude

--- app.js
+++ app.js
@@ -29,7 +29,8 @@ function success(position) {
 }
 
 function error() {
-    alert('Posisi tidak dapat di akses');
+    // default latitude dan longitude jakarta
+    prayerTimes('-6.200000', '106.816666');
 }
 
 function userLocation() {
